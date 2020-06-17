701e451a6a1ca33dad9da5ec5fb46a3632d021d2
Christine Clemens <christineclemens.tesol@gmail.com>
2020-06-16 22:22:44 -0400

    Added methods to Employee class constructor in Employee.js file.

--- Develop/Library/Employee.js
+++ Develop/Library/Employee.js
@@ -4,5 +4,18 @@ class Employee {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.role = role
+        this.role = role;
+    }
+    getName() {
+        return this.name;
+    }
+    getID() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getRole() {
+        return this.role;
+    }
 }
