a938515f2d89230acbb9e2b88ae328a06ca0e7b6
Christine Clemens <christineclemens.tesol@gmail.com>
2020-06-16 22:36:24 -0400

    Added explanatory comments to Employee.js and Engineer.js files.

--- Develop/Library/Engineer.js
+++ Develop/Library/Engineer.js
@@ -1,9 +1,10 @@
 1
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 
-
+//Require the Employee class properties.
 const Employee = require ("./Employee");
 
+//Define Engineer class information and methods by extending those pre-existing in the Employee class.
 class Engineer extends Employee {
     constructor (name, id, email, gitHub) {
         super (name, id, email, "Engineer");
@@ -14,4 +15,5 @@ class Engineer extends Employee {
     }
 }
 
+//Make this information available to other files as a requirement.
 module.exports = Engineer
